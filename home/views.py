efc1a1edad01042b1ebada797507d01b90f2aab7
8825501015 <8825501015@jpu.pdc>
2026-05-20 10:47:16 +0530

    clean project

--- home/views.py
+++ home/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render, HttpResponse
 
 def index(request):
     return render(request,"home.html")
+
+def about(request):
+    return render(request,"about.html")
+
+def jobs(request):
+    return render(request,"jobs.html")
